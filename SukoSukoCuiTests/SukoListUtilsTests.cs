ce8f1ba22339f2487690c4307d3251dd453561d8
AnKoushinist <109f713c@sigaint.org>
2017-09-11 21:20:07 +0900

    SukoList loader with tests

---
+++ SukoSukoCuiTests/SukoListUtilsTests.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SUKOAuto.sukoList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUKOAuto.sukoList.Tests
+{
+    [TestClass]
+    public class SukoListUtilsTests
+    {
+        [TestMethod]
+        public void ParsePlayListTest()
+        {
+            var hikaru = "UUaminwG9MTO4sLYeC3s6udA";
+
+            var parsed = SukoListUtils.Parse(@"
+<SukoList>
+    <!-- PlayList tag with text -->
+    <PlayList>UUaminwG9MTO4sLYeC3s6udA</PlayList>
+    <!-- PlayList tag with channel attribute -->
+    <PlayList playlist=""UUaminwG9MTO4sLYeC3s6udA""/>
+    <!-- PlayList tag with text and maxSuko attribute -->
+    <PlayList maxSuko=""1"">UUaminwG9MTO4sLYeC3s6udA</PlayList>
+    <!-- PlayList tag with channel attribute and maxSuko attribute -->
+    <PlayList maxSuko=""1"" playlist=""UUaminwG9MTO4sLYeC3s6udA""/>
+</SukoList>
+").ToList();
+            var suko0 = parsed[0];
+            Assert.IsTrue(suko0 is PlayListSukoList);
+            Assert.IsTrue((suko0 as PlayListSukoList).PlayList == hikaru);
+            Assert.IsTrue((suko0 as PlayListSukoList).MaxSuko == -1);
+
+            var suko1 = parsed[1];
+            Assert.IsTrue(suko1 is PlayListSukoList);
+            Assert.IsTrue((suko1 as PlayListSukoList).PlayList == hikaru);
+            Assert.IsTrue((suko1 as PlayListSukoList).MaxSuko == -1);
+
+            var suko2 = parsed[2];
+            Assert.IsTrue(suko2 is PlayListSukoList);
+            Assert.IsTrue((suko2 as PlayListSukoList).PlayList == hikaru);
+            Assert.IsTrue((suko2 as PlayListSukoList).MaxSuko == 1);
+
+            var suko3 = parsed[3];
+            Assert.IsTrue(suko3 is PlayListSukoList);
+            Assert.IsTrue((suko3 as PlayListSukoList).PlayList == hikaru);
+            Assert.IsTrue((suko3 as PlayListSukoList).MaxSuko == 1);
+
+        }
+
+        [TestMethod]
+        public void ParseChannelTest()
+        {
+            var hikaru = "UCaminwG9MTO4sLYeC3s6udA";
+            var hikaruPL = "UUaminwG9MTO4sLYeC3s6udA";
+
+            var parsed = SukoListUtils.Parse(@"
+<SukoList>
+    <!-- Channel tag with text -->
+    <Channel>UCaminwG9MTO4sLYeC3s6udA</Channel>
+    <!-- Channel tag with channel attribute -->
+    <Channel channel=""UCaminwG9MTO4sLYeC3s6udA""/>
+    <!-- Channel tag with text and maxSuko attribute -->
+    <Channel maxSuko=""1"">UCaminwG9MTO4sLYeC3s6udA</Channel>
+    <!-- Channel tag with channel attribute and maxSuko attribute -->
+    <Channel maxSuko=""1"" channel=""UCaminwG9MTO4sLYeC3s6udA""/>
+</SukoList>
+").ToList();
+            var suko0 = parsed[0];
+            Assert.IsTrue(suko0 is ChannelSukoList);
+            Assert.IsTrue((suko0 as ChannelSukoList).PlayList == hikaruPL);
+            Assert.IsTrue((suko0 as ChannelSukoList).Channel == hikaru);
+            Assert.IsTrue((suko0 as ChannelSukoList).MaxSuko == -1);
+
+            var suko1 = parsed[1];
+            Assert.IsTrue(suko1 is ChannelSukoList);
+            Assert.IsTrue((suko1 as ChannelSukoList).PlayList == hikaruPL);
+            Assert.IsTrue((suko1 as ChannelSukoList).Channel == hikaru);
+            Assert.IsTrue((suko1 as ChannelSukoList).MaxSuko == -1);
+
+            var suko2 = parsed[2];
+            Assert.IsTrue(suko2 is ChannelSukoList);
+            Assert.IsTrue((suko2 as ChannelSukoList).PlayList == hikaruPL);
+            Assert.IsTrue((suko2 as ChannelSukoList).Channel == hikaru);
+            Assert.IsTrue((suko2 as ChannelSukoList).MaxSuko == 1);
+
+            var suko3 = parsed[3];
+            Assert.IsTrue(suko3 is ChannelSukoList);
+            Assert.IsTrue((suko3 as ChannelSukoList).PlayList == hikaruPL);
+            Assert.IsTrue((suko3 as ChannelSukoList).Channel == hikaru);
+            Assert.IsTrue((suko3 as ChannelSukoList).MaxSuko == 1);
+
+        }
+
+        [TestMethod]
+        public void ParseMoviesVideosTest()
+        {
+            var hikaruGames = new string[] {
+                "XmaIkLIj_tk",
+                "mgrL-cxaJ8A",
+                "1lG_1duX3Uc",
+                "ORxxoLbKq1A",
+                "geHqQt7OxVM",
+                "vouotQqdJDg",
+                "KFlPBerdIGQ",
+                "pSN0S9pQZq4",
+                "5y28JiFcudo",
+                "Nvv_t_CEVIQ",
+            };
+
+            var raphael = new string[] {
+                "gZsXyLn2BfY",
+                "zKoZSZDmtFU",
+                "4UxwFZFrbKQ",
+                "Es0u39q-rLU",
+                "1FhPn8LIqkY",
+                "47TeW-jLzyQ",
+                "DRqgkgLfkL0",
+                "SfX5QGkc--A",
+                "OqSQ19UWOn4",
+                "WAJji7z-49U",
+            };
+
+            var parsed = SukoListUtils.Parse(@"
+<SukoList>
+    <!-- Movies tag -->
+    <Movies>
+        <Movie>XmaIkLIj_tk</Movie>
+        <Movie>mgrL-cxaJ8A</Movie>
+        <Movie>1lG_1duX3Uc</Movie>
+        <Movie>ORxxoLbKq1A</Movie>
+        <Movie>geHqQt7OxVM</Movie>
+        <Movie>vouotQqdJDg</Movie>
+        <Movie>KFlPBerdIGQ</Movie>
+        <Movie>pSN0S9pQZq4</Movie>
+        <Movie>5y28JiFcudo</Movie>
+        <Movie>Nvv_t_CEVIQ</Movie>
+    </Movies>
+    <!-- Videos tag -->
+    <Videos>
+        <Video>gZsXyLn2BfY</Video>
+        <Video>zKoZSZDmtFU</Video>
+        <Video>4UxwFZFrbKQ</Video>
+        <Video>Es0u39q-rLU</Video>
+        <Video>1FhPn8LIqkY</Video>
+        <Video>47TeW-jLzyQ</Video>
+        <Video>DRqgkgLfkL0</Video>
+        <Video>SfX5QGkc--A</Video>
+        <Video>OqSQ19UWOn4</Video>
+        <Video>WAJji7z-49U</Video>
+    </Videos>
+</SukoList>
+").ToList();
+            var suko0 = parsed[0];
+            Assert.IsTrue(suko0 is ConstantSukoList);
+            Assert.IsTrue((suko0 as ConstantSukoList).GetProcessedMovies()
+                .SequenceEqual(hikaruGames));
+
+            var suko1 = parsed[1];
+            Assert.IsTrue(suko1 is ConstantSukoList);
+            Assert.IsTrue((suko1 as ConstantSukoList).GetProcessedMovies()
+                 .SequenceEqual(raphael));
+        }
+
+        [TestMethod]
+        public void ParseOmaturiTest()
+        {
+            var parsed = SukoListUtils.Parse(@"
+<SukoList>
+    <!-- Omaturi tag -->
+    <Omaturi/>
+</SukoList>
+").ToList();
+            var suko0 = parsed[0];
+            Assert.IsTrue(suko0 is OmaturiSukoList);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+            "\"omaturi\"は2回以上入れられません。")]
+        public void ParseMultipleOmaturiTest()
+        {
+            var parsed = SukoListUtils.Parse(@"
+<SukoList>
+    <!-- Omaturi tag -->
+    <Omaturi/>
+    <!-- Again: it will cause an error -->
+    <Omaturi/>
+</SukoList>
+");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+                "チャンネル\"UUaminwG9MTO4sLYeC3s6udA\"は不正です。再生リストと間違えていませんか?")]
+        public void ParseIllegalChannelTest()
+        {
+            var parsed = SukoListUtils.Parse(@"
+<SukoList>
+    <!-- Channel tag with illegal value (use PlayList form) -->
+    <Channel channel=""UUaminwG9MTO4sLYeC3s6udA""/>
+</SukoList>
+");
+        }
+    }
+}
---
+++ cui/SukoList.cs
@@ -0,0 +1,229 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace SUKOAuto.sukoList
+{
+    public interface ISukoListEntry
+    {
+        bool IsConstant();
+        void SetMovies(string[] movies);
+        string[] GetMovies();
+        IEnumerable<string> GetProcessedMovies();
+    }
+
+    public abstract class AbstractSukoListEntry : ISukoListEntry
+    {
+        private string[] Movies;
+
+        public abstract IEnumerable<string> GetProcessedMovies();
+        public abstract bool IsConstant();
+
+        public virtual string[] GetMovies()
+        {
+            return Movies;
+        }
+
+        public virtual void SetMovies(string[] movies)
+        {
+            Movies=movies;
+        }
+    }
+
+    /**
+     * チャンネル(ただしWebすこ砲と同じ方法でプレイリスト化して扱う)
+     */
+    public class ChannelSukoList : PlayListSukoList
+    {
+        private string ChannelId;
+
+        public ChannelSukoList(string PlayList, int Head = -1) : base(ChannelToPlayList(PlayList),Head)
+        {
+            ChannelId = PlayList;
+        }
+        
+        static string ChannelToPlayList(string Channel)
+        {
+            if (!Channel.StartsWith("UC"))
+                throw new ArgumentException($"チャンネル\"{Channel}\"は不正です。再生リストと間違えていませんか?");
+            var Buf=Channel.ToCharArray();
+            Buf[1] = 'U';
+            return new string(Buf);
+        }
+
+        public string Channel => ChannelId;
+    }
+
+    /**
+     * 再生リスト
+     */
+    public class PlayListSukoList : AbstractSukoListEntry
+    {
+        private string PlayListId;
+        private int Head = -1;
+
+        public PlayListSukoList(string PlayList, int Head = -1)
+        {
+            this.PlayListId = PlayList;
+            this.Head = Head;
+        }
+
+        public override IEnumerable<string> GetProcessedMovies()
+        {
+            return Head<0? GetMovies():GetMovies().Take(Head);
+        }
+
+        public override bool IsConstant()
+        {
+            return false;
+        }
+
+        public string PlayList => PlayListId;
+        public int MaxSuko => Head;
+    }
+
+    /**
+     * 狙い撃ち
+     */
+    public class ConstantSukoList : AbstractSukoListEntry
+    {
+        string[] Movies;
+
+        public ConstantSukoList(string[] Movies) {
+            this.Movies = Movies;
+        }
+
+        public override string[] GetMovies() {
+            return Movies;
+        }
+
+        public override IEnumerable<string> GetProcessedMovies()
+        {
+            return Movies;
+        }
+
+        public override bool IsConstant()
+        {
+            return true;
+        }
+    }
+
+    /**
+     * Webすこ砲のomaturi
+     */
+    public class OmaturiSukoList : AbstractSukoListEntry
+    {
+        public override string[] GetMovies()
+        {
+            JObject json;
+            using (var wc=new WebClient()) {
+                json = JObject.Parse(wc.DownloadString("http://websuko.xyz/v1/target.php"));
+            }
+            return json["videos"].Select(a=>(string)a).ToArray();
+        }
+
+        public override IEnumerable<string> GetProcessedMovies()
+        {
+            return GetMovies();
+        }
+
+        public override bool IsConstant()
+        {
+            return false;
+        }
+    }
+
+    public class SukoListUtils {
+        public static IEnumerable<ISukoListEntry> Parse(TextReader reader) {
+            XDocument doc = XDocument.Load(reader);
+            var root = doc.Element("SukoList");
+            var elements = root.Elements();
+            var entries = new List<ISukoListEntry>();
+            var omaturiAdded = false;
+            foreach (var element in elements)
+            {
+                switch (element.Name.LocalName) {
+                    case "omaturi":
+                    case "Omaturi":
+                        if (omaturiAdded)
+                            throw new ArgumentException("\"omaturi\"は2回以上入れられません。");
+                        entries.Add(new OmaturiSukoList());
+                        omaturiAdded = true;
+                        break;
+                    case "movies":
+                    case "Movies":
+                    case "videos":
+                    case "Videos":
+                        var regex = "^([vV]ideo|[mM]ovie)$";
+                        var Movies = element.Elements()
+                            .Where(a=>Regex.IsMatch(a.Name.LocalName,regex))
+                            .Select(a=>a.Value)
+                            .ToArray();
+                        entries.Add(new ConstantSukoList(Movies));
+                        break;
+                    case "Channel":
+                    case "channel":
+                        {
+                            int Head = -1;
+                            if (element.Attribute("count") != null)
+                            {
+                                Head = int.Parse(element.Attribute("count").Value);
+                            }
+                            else if (element.Attribute("maxSuko") != null)
+                            {
+                                Head = int.Parse(element.Attribute("maxSuko").Value);
+                            }
+                            string Channel;
+                            if (element.Attribute("channel") != null)
+                            {
+                                Channel = element.Attribute("channel").Value;
+                            }
+                            else
+                            {
+                                Channel = element.Value;
+                            }
+                            entries.Add(new ChannelSukoList(Channel, Head));
+                        }
+                        break;
+                    case "Playlist":
+                    case "playlist":
+                    case "PlayList":
+                    case "playList":
+                        {
+                            int Head = -1;
+                            if (element.Attribute("count") != null)
+                            {
+                                Head = int.Parse(element.Attribute("count").Value);
+                            }
+                            else if (element.Attribute("maxSuko") != null)
+                            {
+                                Head = int.Parse(element.Attribute("maxSuko").Value);
+                            }
+                            string PlayList;
+                            if (element.Attribute("playlist") != null)
+                            {
+                                PlayList = element.Attribute("playlist").Value;
+                            }
+                            else
+                            {
+                                PlayList = element.Value;
+                            }
+                            entries.Add(new PlayListSukoList(PlayList, Head));
+                        }
+                        break;
+                }
+            }
+            return entries;
+        }
+
+        public static IEnumerable<ISukoListEntry> Parse(string xml)
+        {
+            return Parse(new StringReader(xml));
+        }
+    }
+}
